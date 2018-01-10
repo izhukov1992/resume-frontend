1c491b921fb8af5f3431ed6098f21959cae4c6c5
Ilya Zhukov <izhukov1992@gmail.com>
2018-01-10 11:14:27 +0300

    Start react project

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
 
 gulp.task('copy', function() {
   gulp.src('node_modules/startbootstrap-resume/vendor/**/*')
@@ -14,4 +17,17 @@ gulp.task('copy', function() {
     .pipe(gulp.dest('assets/img'))
 });
 
-gulp.task('default', ['copy']);
+gulp.task("build", function(){
+  return browserify({
+    entries: ["./src/index.js"],
+    extensions: [".js", ".jsx"]
+  })
+  .transform(babelify.configure({
+    presets : ["es2015", "react"]
+  }))
+  .bundle()
+  .pipe(source("bundle.js"))
+  .pipe(gulp.dest("./assets/js"));
+});
+
+gulp.task('default', ['copy', 'build']);
